374e98bc34cb3e97270ff66fd980b799d4ecf7e3
Navid shad <navidshad72@gmail.com>
2018-09-20 11:59:01 +0430

    add port and onAfterInit

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ var router = require('koa-router');
 var path = require('path');
 var combination = require('./combination');
 
-module.exports = (option={}) => 
+module.exports = function(option={}) 
 {
     var app = new koa();
 
     var root = (option.root) ? option.root : null;
     var onInit = (option.onInit) ? option.onInit : null;
+    var onAfterInit = (option.onAfterInit) ? option.onAfterInit : null;
+    var port = (option.port) ? option.port : 3000;
 
-    // var options = {
+    // var option = {
     //     'root': path.join( __dirname, 'routes'),
     // }
 
-    if(root) combination.combinRoutes(options.root, app);
+    if(root) combination.combinRoutes(option.root, app);
     if(onInit) onInit(app);
 
-    app.listen(3000);
+    app.listen(port);
+
+    // on affter init
+    if(onAfterInit) onAfterInit();
 }
