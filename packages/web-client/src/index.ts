be24ec875b5c5caec3d56c22e46658f480addee8
Navid Shad <navidshad72@gmail.com>
2020-11-13 13:12:25 +0330

    some preparation for publish

---
+++ packages/web-client/src/index.ts
@@ -0,0 +1,7 @@
+import DataProvider from './services/data-provider';
+import Authentication from './services/authentication';
+import GlobalOptions from './class/global_options';
+
+export {
+    GlobalOptions, Authentication, DataProvider,
+}
