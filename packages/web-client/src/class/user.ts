7d5a93923f460379514ffb103cc96abf6996acd5
Navid Shad <navidshad72@gmail.com>
2020-12-04 12:22:21 +0330

    web - added user entity

---
+++ packages/web-client/src/class/user.ts
@@ -0,0 +1,34 @@
+class User {
+
+    email?: string;
+    phone?: string;
+    id: string;
+    /**
+     * Get permission type
+     */
+    type: string;
+    private permission: any;
+
+    constructor(detail: { email?: string, phone?: string, id: string, permission: any }) {
+        if (detail.email)
+            this.email = detail.email;
+        if (detail.phone)
+            this.phone = detail.phone;
+
+        this.id = detail.id;
+        this.permission = detail.permission;
+        this.type = detail.permission.title;
+    }
+
+    /**
+     * Check whether or not the user has access to an specific permission.
+     * @param permissionName permission name
+     */
+    hasAccess(permissionName: string) {
+        if (this.permission[permissionName] != undefined)
+            return this.permission[permissionName];
+        else return false;
+    }
+}
+
+export default User;
