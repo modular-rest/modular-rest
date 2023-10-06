e2987df3f982c0298320fa95dc91561176f2364a
Navid Shad <navidshad72@gmail.com>
2023-10-06 19:29:18 +0300

    [ server ] changed keypair generator module

--- packages/server/src/helper/presetup_services.js
+++ packages/server/src/helper/presetup_services.js
@@ -1,34 +1,32 @@
-let DataInsertion = require('./data_insertion');
-let JWT = require('../services/jwt/service');
-let FileService = require('../services/file/service');
+let DataInsertion = require("./data_insertion");
+let JWT = require("../services/jwt/service");
+let FileService = require("../services/file/service");
 
 module.exports.setup = async ({ keypair, adminUser, uploadDirectory }) => {
+  /**
+   * Json web Token
+   *
+   * Setup private and public keys for JWT module
+   */
+  if (!keypair) {
+    // generate new keypair
+    const generateKeypair = require("keypair");
+    keypair = generateKeypair();
+  }
 
-    /**
-     * Json web Token
-     * 
-     * Setup private and public keys for JWT module 
-     */
-    if (!keypair) {
-        // generate new keypair
-        var generateRSAKeypair = require('generate-rsa-keypair')
-        keypair = generateRSAKeypair()
-    }
+  JWT.main.setKies(keypair.private, keypair.public);
 
-    JWT.main.setKies(keypair.private, keypair.public);
+  /**
+   * Data Insertion
+   *
+   * Insert permissions and admin user
+   * for the first time
+   */
+  await DataInsertion.createPermissions();
+  await DataInsertion.createAdminUser(adminUser);
 
-    /**
-     * Data Insertion
-     * 
-     * Insert permissions and admin user
-     * for the first time
-     */
-    await DataInsertion.createPermissions();
-    await DataInsertion.createAdminUser(adminUser)
-
-    /**
-     * File Service
-     */
-    FileService.setUploadDirectory(uploadDirectory);
-
-}
+  /**
+   * File Service
+   */
+  FileService.setUploadDirectory(uploadDirectory);
+};
