08a854a69ddbfd26f942e50fd95f0a4f35100e0b
Navid Shad <navidshad72@gmail.com>
2023-10-09 19:49:38 +0300

    [ server ] add password encodding for users

--- packages/server/src/services/user_manager/db.js
+++ packages/server/src/services/user_manager/db.js
@@ -1,62 +1,72 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-let CollectionDefinition = require('../../class/collection_definition');
-let { Permission, PermissionTypes } = require('../../class/security');
+let CollectionDefinition = require("../../class/collection_definition");
+let { Permission, PermissionTypes } = require("../../class/security");
 
 /**
  * Permission schema
- * 
+ *
  * This schema is generated dynamically
  * by combining default & custom permissions.
  */
 let permissionSchemaConstructorOption = {
-    title: String,
-    isAnonymous: { type: Boolean, default: false },
-    isDefault: { type: Boolean, default: false },
+  title: String,
+  isAnonymous: { type: Boolean, default: false },
+  isDefault: { type: Boolean, default: false },
 };
-Object.keys(new PermissionTypes())
-    .forEach(key => {
-        let fieldOption = { type: Boolean, default: false };
-        permissionSchemaConstructorOption[key] = fieldOption;
-    })
+Object.keys(new PermissionTypes()).forEach((key) => {
+  let fieldOption = { type: Boolean, default: false };
+  permissionSchemaConstructorOption[key] = fieldOption;
+});
 
 let permissionSchema = new Schema(permissionSchemaConstructorOption);
 permissionSchema.index({ title: 1 }, { unique: true });
 
 let authSchema = new Schema({
-    permission: { type: Schema.Types.ObjectId, ref: 'permission', required: false },
-    email: String,
-    phone: String,
-    password: String,
-    type: { type: String, default: 'user', enum: ['user', 'anonymous'] }
+  permission: {
+    type: Schema.Types.ObjectId,
+    ref: "permission",
+    required: false,
+  },
+  email: String,
+  phone: String,
+  password: String,
+  type: { type: String, default: "user", enum: ["user", "anonymous"] },
 });
 authSchema.index({ email: 1 }, { unique: true });
+authSchema.pre(["save", "updateOne"], function (next) {
+  // Encode the password before saving
+  if (this.isModified("password")) {
+    this.password = Buffer.from(this.password).toString("base64");
+  }
+  next();
+});
 
 module.exports = [
-    new CollectionDefinition({
-        db: 'cms',
-        collection: 'auth',
-        schema: authSchema,
-        permissions: [
-            new Permission({
-                type: PermissionTypes.advanced_settings,
-                read: true,
-                write: true
-            }),
-        ]
-    }),
+  new CollectionDefinition({
+    db: "cms",
+    collection: "auth",
+    schema: authSchema,
+    permissions: [
+      new Permission({
+        type: PermissionTypes.advanced_settings,
+        read: true,
+        write: true,
+      }),
+    ],
+  }),
 
-    new CollectionDefinition({
-        db: 'cms',
-        collection: 'permission',
-        schema: permissionSchema,
-        permissions: [
-            new Permission({
-                type: PermissionTypes.advanced_settings,
-                read: true,
-                write: true
-            }),
-        ],
-    }),
-]
+  new CollectionDefinition({
+    db: "cms",
+    collection: "permission",
+    schema: permissionSchema,
+    permissions: [
+      new Permission({
+        type: PermissionTypes.advanced_settings,
+        read: true,
+        write: true,
+      }),
+    ],
+  }),
+];
--- packages/server/src/services/user_manager/service.js
+++ packages/server/src/services/user_manager/service.js
@@ -1,271 +1,275 @@
-let User = require('../../class/user');
-const DataProvider = require('../data_provider/service')
-const JWT = require('../jwt/service')
+let User = require("../../class/user");
+const DataProvider = require("../data_provider/service");
+const JWT = require("../jwt/service");
 
 class UserManager {
+  constructor() {
+    this.tempIds = {};
+  }
 
-    constructor() {
-        this.tempIds = {};
+  /**
+   *
+   * @param {function} method a method that return random verification code
+   */
+  setCustomVerificationCodeGeneratorMethod(method) {
+    this.verificationCodeGeneratorMethod = method;
+  }
+
+  generateVerificationCode(id, idType) {
+    if (this.verificationCodeGeneratorMethod)
+      return this.verificationCodeGeneratorMethod(id, idType);
+
+    // this is default code
+    return "123";
+  }
+
+  /**
+   * Get a user by its Id.
+   *
+   * @param {string} id userid
+   */
+  getUserById(id) {
+    return new Promise(async (done, reject) => {
+      let userModel = DataProvider.getCollection("cms", "auth");
+
+      let userDoc = await userModel
+        .findOne({ _id: id })
+        .select({ password: 0 })
+        .populate("permission")
+        .exec()
+        .catch(reject);
+
+      if (!userDoc) reject("user not found");
+
+      let user = User.loadFromModel(userDoc);
+      done(user);
+    });
+  }
+
+  /**
+   * Get user by token.
+   *
+   * @param {string} token auth token
+   */
+  getUserByToken(token) {
+    return JWT.main.verify(token).then(async (payload) => {
+      let user = payload;
+      let permission = await DataProvider.getCollection("cms", "permission")
+        .findOne({ _id: user.permission })
+        .exec()
+        .then();
+
+      if (!permission) throw "user has a wrong permission";
+
+      user.permission = permission;
+      return user;
+    });
+  }
+
+  /**
+   * Define whether or not verification code is valid.
+   *
+   * @param {string} id auth id phone|email
+   * @param {string} code verification code
+   */
+  isCodeValid(id, code) {
+    let key = false;
+
+    if (
+      this.tempIds.hasOwnProperty(id) &&
+      this.tempIds[id].code.toString() === code.toString()
+    )
+      key = true;
+
+    return key;
+  }
+
+  /**
+   * Login and return user token.
+   *
+   * @param {string} id auth id phone|email
+   * @param {string} idType auth type phone|email
+   * @param {string} password
+   */
+  loginUser(id = "", idType = "", password = "") {
+    let token;
+
+    return new Promise(async (done, reject) => {
+      // Get user model
+      let userModel = DataProvider.getCollection("cms", "auth");
+
+      /**
+       * Setup query to find by phone or email
+       */
+      let query = {
+        password: Buffer.from(password).toString("base64"),
+        type: "user",
+      };
+
+      if (idType == "phone") query["phone"] = id;
+      else if (idType == "email") query["email"] = id;
+
+      // Get from database
+      let gottenFromDB = await userModel
+        .findOne(query)
+        .populate("permission")
+        .exec()
+        .catch(reject);
+
+      if (!gottenFromDB) reject("user not found");
+      // Token
+      else {
+        // Load user
+        let user = await User.loadFromModel(gottenFromDB).then().catch(reject);
+
+        // Get token payload
+        // This is some information about the user.
+        let payload = user.getBrief();
+
+        // Generate json web token
+        token = await JWT.main.sign(payload).then().catch(reject);
+
+        done(token);
+      }
+    });
+  }
+
+  /**
+   * Login as anonymous user
+   */
+  loginAnonymous() {
+    let token;
+
+    return new Promise(async (done, reject) => {
+      // Get user model
+      let userModel = DataProvider.getCollection("cms", "auth");
+
+      // Setup query
+      let query = { type: "anonymous" };
+
+      // Get from database
+      let gottenFromDB = await userModel
+        .findOne(query)
+        .populate("permission")
+        .exec()
+        .then()
+        .catch(reject);
+
+      // Create a new anonymous user if it doesn't exist.
+      // There are only one anonymous user in the database
+      // and every guest token being generated from it.
+      if (!gottenFromDB) {
+        let newUserId = await this.registerUser({ type: "anonymous" }).catch(
+          reject
+        );
+        gottenFromDB = await this.getUserById(newUserId).catch(reject);
+      }
+
+      // load User
+      let user = await User.loadFromModel(gottenFromDB).then().catch(reject);
+
+      // Get token payload
+      // This is some information about the user.
+      let payload = user.getBrief();
+
+      // Generate json web token
+      token = await JWT.main.sign(payload).then().catch(reject);
+
+      done(token);
+    });
+  }
+
+  /**
+   * Store user email|phone temporarily in memory.
+   *
+   * @param {string} id id is username or phone number
+   * @param {string} type type is the type of id
+   * @param {string} code code is a string being sent to user and he/she must return it back.
+   */
+  registerTemporaryID(id, type, code) {
+    this.tempIds[id] = { id: id, type: type, code: code };
+  }
+
+  async submitPasswordForTemporaryID(id, password, code) {
+    let key = false;
+
+    // If user email|phone has already stored
+    // a new user being created
+    if (
+      this.tempIds.hasOwnProperty(id) &&
+      this.tempIds[id].code.toString() == code.toString()
+    ) {
+      let authDetail = { password: password };
+
+      if (this.tempIds[id].type == "phone") authDetail["phone"] = id;
+      else if (this.tempIds[id].type == "email") authDetail["email"] = id;
+
+      await this.registerUser(authDetail)
+        .then(() => (key = true))
+        .catch((e) => console.log(e));
     }
 
-    /**
-     * 
-     * @param {function} method a method that return random verification code
-     */
-    setCustomVerificationCodeGeneratorMethod(method) {
-        this.verificationCodeGeneratorMethod = method
+    delete this.tempIds[id];
+    return key;
+  }
+
+  async changePasswordForTemporaryID(id, password, code) {
+    let key = false;
+
+    if (this.tempIds.hasOwnProperty(id) && this.tempIds[id].code == code) {
+      let query = {};
+
+      if (this.tempIds[id].type == "phone") query["phone"] = id;
+      else if (this.tempIds[id].type == "email") query["email"] = id;
+
+      await this.changePassword(query, password)
+        .then(() => (key = true))
+        .catch((e) => console.log(e));
     }
 
-    generateVerificationCode(id, idType) {
+    delete this.tempIds[id];
+    return key;
+  }
 
-        if (this.verificationCodeGeneratorMethod)
-            return this.verificationCodeGeneratorMethod(id, idType)
+  registerUser(detail) {
+    return new Promise(async (done, reject) => {
+      // get default permission
+      let permissionId;
+      let perM = DataProvider.getCollection("cms", "permission");
 
-        // this is default code 
-        return '123'
-    }
+      let pQuery = { isDefault: true };
 
+      if (detail.type == "anonymous") pQuery = { isAnonymous: true };
 
-    /**
-     * Get a user by its Id.
-     * 
-     * @param {string} id userid
-     */
-    getUserById(id) {
-        return new Promise(async (done, reject) => {
-            let userModel = DataProvider.getCollection('cms', 'auth');
+      await perM
+        .findOne(pQuery, "_id")
+        .exec()
+        .then((doc) => (permissionId = doc._id))
+        .catch(reject);
 
-            let userDoc = await userModel.findOne({ '_id': id }).select({ password: 0 })
-                .populate('permission').exec().catch(reject);
+      detail.permission = permissionId;
 
-            if (!userDoc) reject('user not found');
+      let authM = DataProvider.getCollection("cms", "auth");
+      return User.createFromModel(authM, detail)
+        .then((newUser) => {
+          DataProvider.triggers.call("insertOne", "cms", "auth", {
+            input: detail,
+            output: newUser.dbModel,
+          });
 
-            let user = User.loadFromModel(userDoc);
-            done(user);
+          done(newUser.id);
         })
-    }
+        .catch(reject);
+    });
+  }
 
-    /**
-     * Get user by token.
-     * 
-     * @param {string} token auth token
-     */
-    getUserByToken(token) {
-        return JWT.main.verify(token)
-            .then(async payload => {
-                let user = payload;
-                let permission = await DataProvider.getCollection('cms', 'permission')
-                    .findOne({ _id: user.permission }).exec().then();
-
-                if (!permission) throw ('user has a wrong permission');
-
-                user.permission = permission;
-                return user;
-            });
-    }
-
-    /**
-     * Define whether or not verification code is valid.
-     * 
-     * @param {string} id auth id phone|email
-     * @param {string} code verification code
-     */
-    isCodeValid(id, code) {
-        let key = false;
-
-        if (this.tempIds.hasOwnProperty(id) && this.tempIds[id].code.toString() === code.toString())
-            key = true;
-
-        return key;
-    }
-
-    /**
-     * Login and return user token.
-     * 
-     * @param {string} id auth id phone|email
-     * @param {string} idType auth type phone|email
-     * @param {string} password 
-     */
-    loginUser(id = '', idType = '', password = '') {
-        let token;
-
-        return new Promise(async (done, reject) => {
-
-            // Get user model
-            let userModel = DataProvider.getCollection('cms', 'auth');
-
-            /**
-             * Setup query to find by phone or email
-             */
-            let query = { 'password': password, type: "user" };
-
-            if (idType == 'phone') query['phone'] = id;
-            else if (idType == 'email') query['email'] = id;
-
-            // Get from database
-            let gottenFromDB = await userModel
-                .findOne(query).populate('permission')
-                .exec().catch(reject);
-
-            if (!gottenFromDB) reject('user not found');
-
-            // Token
-            else {
-
-                // Load user
-                let user = await User.loadFromModel(gottenFromDB)
-                    .then().catch(reject);
-
-                // Get token payload
-                // This is some information about the user.
-                let payload = user.getBrief();
-
-                // Generate json web token
-                token = await JWT.main.sign(payload)
-                    .then().catch(reject);
-
-                done(token);
-            }
-        });
-    }
-
-    /**
-     * Login as anonymous user
-     */
-    loginAnonymous() {
-        let token;
-
-        return new Promise(async (done, reject) => {
-
-            // Get user model
-            let userModel = DataProvider.getCollection('cms', 'auth');
-
-            // Setup query
-            let query = { 'type': 'anonymous' };
-
-            // Get from database
-            let gottenFromDB = await userModel
-                .findOne(query).populate('permission')
-                .exec().then().catch(reject);
-
-            // Create a new anonymous user if it doesn't exist.
-            // There are only one anonymous user in the database
-            // and every guest token being generated from it.
-            if (!gottenFromDB) {
-                let newUserId = await this.registerUser({ 'type': 'anonymous' }).catch(reject);
-                gottenFromDB = await this.getUserById(newUserId).catch(reject);
-            }
-
-            // load User
-            let user = await User.loadFromModel(gottenFromDB)
-                .then().catch(reject);
-
-            // Get token payload
-            // This is some information about the user.
-            let payload = user.getBrief();
-
-            // Generate json web token
-            token = await JWT.main.sign(payload)
-                .then().catch(reject);
-
-            done(token);
-        });
-    }
-
-    /**
-     * Store user email|phone temporarily in memory.
-     *  
-     * @param {string} id id is username or phone number
-     * @param {string} type type is the type of id
-     * @param {string} code code is a string being sent to user and he/she must return it back.
-     */
-    registerTemporaryID(id, type, code) {
-        this.tempIds[id] = { 'id': id, 'type': type, 'code': code };
-    }
-
-    async submitPasswordForTemporaryID(id, password, code) {
-        let key = false;
-
-        // If user email|phone has already stored
-        // a new user being created 
-        if (
-            this.tempIds.hasOwnProperty(id) &&
-            this.tempIds[id].code.toString() == code.toString()
-        ) {
-
-            let authDetail = { 'password': password };
-
-            if (this.tempIds[id].type == 'phone')
-                authDetail['phone'] = id;
-            else if (this.tempIds[id].type == 'email')
-                authDetail['email'] = id;
-
-            await this.registerUser(authDetail)
-                .then(() => key = true)
-                .catch((e) => console.log(e));
-        }
-
-        delete this.tempIds[id];
-        return key;
-    }
-
-    async changePasswordForTemporaryID(id, password, code) {
-        let key = false;
-
-        if (this.tempIds.hasOwnProperty(id)
-            && this.tempIds[id].code == code) {
-            let query = {};
-
-            if (this.tempIds[id].type == 'phone')
-                query['phone'] = id;
-            else if (this.tempIds[id].type == 'email')
-                query['email'] = id;
-
-            await this.changePassword(query, password)
-                .then(() => key = true)
-                .catch((e) => console.log(e));
-        }
-
-        delete this.tempIds[id];
-        return key;
-    }
-
-    registerUser(detail) {
-        return new Promise(async (done, reject) => {
-            // get default permission
-            let permissionId;
-            let perM = DataProvider.getCollection('cms', 'permission');
-
-            let pQuery = { isDefault: true };
-
-            if (detail.type == 'anonymous')
-                pQuery = { isAnonymous: true };
-
-            await perM.findOne(pQuery, '_id').exec()
-                .then((doc) => permissionId = doc._id)
-                .catch(reject);
-
-            detail.permission = permissionId;
-
-            let authM = DataProvider.getCollection('cms', 'auth');
-            return User.createFromModel(authM, detail)
-                .then(newUser => {
-                    DataProvider.triggers
-                        .call('insertOne', 'cms', 'auth', { 'input': detail, 'output': newUser.dbModel });
-
-                    done(newUser.id);
-                })
-                .catch(reject);
-        });
-    }
-
-    changePassword(query, newPass) {
-        let update = { '$set': { 'password': newPass } };
-        let authM = DataProvider.getCollection('cms', 'auth');
-        return authM.updateOne(query, update).exec().then();
-    }
+  changePassword(query, newPass) {
+    let update = { $set: { password: newPass } };
+    let authM = DataProvider.getCollection("cms", "auth");
+    return authM.updateOne(query, update).exec().then();
+  }
 }
 
-UserManager.instance = new UserManager()
-module.exports.name = 'userManager';
+UserManager.instance = new UserManager();
+module.exports.name = "userManager";
 module.exports.main = UserManager.instance;
